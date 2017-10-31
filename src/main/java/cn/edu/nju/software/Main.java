484ac964c9cfb610901311ee92a1fa8110a98218
dalecgu <1677160278@qq.com>
2017-10-31 20:36:24 +0800

    update

--- src/main/java/cn/edu/nju/software/Main.java
+++ src/main/java/cn/edu/nju/software/Main.java
@@ -1,10 +1,12 @@
 package cn.edu.nju.software;
 
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 import com.mongodb.spark.MongoSpark;
-import com.mongodb.spark.config.WriteConfig;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.graphx.Edge;
@@ -19,6 +21,10 @@ import static org.apache.spark.storage.StorageLevel.MEMORY_ONLY;
  */
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
+    private static final String URI = "mongodb://192.168.1.102:27017";
+    private static final String DATABASE = "test";
+    private static final String COLLECTION = "graph";
+    private static final String MONGO_URI = URI + "/" + DATABASE + "." + COLLECTION;
 
     public static void main(String[] args) {
         if (args.length < 1) {
@@ -28,16 +34,19 @@ public class Main {
 
         SparkConf conf = new SparkConf()
                 .setAppName("GraphX")
-                .set("spark.mongodb.input.uri", "mongodb://192.168.1.102:27017/test.graph")
-                .set("spark.mongodb.output.uri", "mongodb://192.168.1.102:27017/test.graph");
-        WriteConfig writeConfig = WriteConfig.create(conf);
+                .set("spark.mongodb.input.uri", MONGO_URI)
+                .set("spark.mongodb.output.uri", MONGO_URI);
         JavaSparkContext context = new JavaSparkContext(conf);
 
-        JavaRDD<Edge<Integer>> edges = context.textFile(args[0])
+        JavaPairRDD<Tuple2<Long, Long>, Integer> initial = MongoSpark.load(context)
+                .mapToPair(document -> new Tuple2<>(new Tuple2<>((long) document.getString("from").charAt(0), (long) document.getString("to").charAt(0)), document.getInteger("count")));
+        JavaPairRDD<Tuple2<Long, Long>, Integer> append = context.textFile(args[0])
                 .filter(line -> line.length() > 2)
-                .mapToPair(line -> new Tuple2<>(new Tuple2<>(line.charAt(0), line.charAt(2)), 1))
+                .mapToPair(line -> new Tuple2<>(new Tuple2<>((long) line.charAt(0), (long) line.charAt(2)), 1));
+        JavaRDD<Edge<Integer>> edges = initial
+                .union(append)
                 .reduceByKey((i1, i2) -> i1 + i2)
-                .map(tuple -> new Edge<>(tuple._1._1, tuple._1._2, tuple._2));
+                .map(tuple -> new Edge<>(tuple._1._1(), tuple._1._2(), tuple._2));
         Graph<Long, Integer> graph = Graph.fromEdges(edges.rdd(),
                 0L,
                 MEMORY_ONLY(),
@@ -47,13 +56,23 @@ public class Main {
 
         JavaRDD<Document> documents = graph.edges()
                 .toJavaRDD()
-                .map(edge ->
-                        Document.parse(String.format("{\"from\": \"%s\", \"to\": \"%s\", \"count\": %d}",
-                                String.valueOf((char) edge.srcId()),
-                                String.valueOf((char) edge.dstId()),
-                                edge.attr)));
-        MongoSpark.save(documents, writeConfig);
+                .map(edge -> {
+                    String from = String.valueOf((char) edge.srcId());
+                    String to = String.valueOf((char) edge.dstId());
+                    return Document.parse(String.format("{\"_id\": \"%s\", \"from\": \"%s\", \"to\": \"%s\", \"count\": %d}",
+                                    from+to,
+                                    from,
+                                    to,
+                                    edge.attr));
+                });
+        logger.info(documents.count());
+        dropCollection(URI, DATABASE, COLLECTION);
+        MongoSpark.save(documents);
 
         context.stop();
+    }
+
+    private static void dropCollection(final String uri, final String database, final String collection) {
+        new MongoClient(new MongoClientURI(uri)).getDatabase(database).getCollection(collection).drop();
     }
 }
